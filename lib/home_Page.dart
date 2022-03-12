c80033c4f668fab46e71828a727e5117f0edd5b5
AyuOrniThrONE <ayushprakashthaware@gmail.com>
2022-03-12 16:53:43 +0530

    Day 2 Completed

---
+++ lib/home_Page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    String name = "CodePur1234";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days Days of Flutter by $name "),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_Page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,13 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Material(
-      child: Center(
-        child: Container(
-          child: Text("Welcome to 30 Days of Flutter"),
-        ),
-      ),
-    ));
+    return MaterialApp(home: HomePage());
   }
 }
