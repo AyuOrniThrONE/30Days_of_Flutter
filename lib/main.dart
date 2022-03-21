f1285e9b81d6fcf4770872352ddb111d92379e82
AyuOrniThrONE <ayushprakashthaware@gmail.com>
2022-03-21 16:51:28 +0530

    changed main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_Page.dart';
 import 'package:flutter_catalog/pages/loginPAge.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,7 +15,6 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.light,
       theme: ThemeData(
         primarySwatch: Colors.lightGreen,
-        fontFamily: GoogleFonts.lato().fontFamily,
       ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
