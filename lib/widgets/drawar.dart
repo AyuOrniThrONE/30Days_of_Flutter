d3b81a93d6d571032ff6d9b051275b1e31d02e98
AyuOrniThrONE <ayushprakashthaware@gmail.com>
2022-03-24 02:20:34 +0530

    Day 8 completed

---
+++ lib/widgets/drawar.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/cupertino.dart';
+import "package:flutter/material.dart";
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final imageurl =
+        "https://yt3.ggpht.com/ytc/AKedOLTuKI-5AD6830oTEpDiWjfhW-OIMtJz9dN0LnCy=s900-c-k-c0x00ffffff-no-rj";
+    return Drawer(
+      child: Container(
+        color: Color.fromARGB(255, 37, 54, 16),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+                padding: EdgeInsets.zero,
+                child: UserAccountsDrawerHeader(
+                  margin: EdgeInsets.zero,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 37, 54, 16),
+                  ),
+                  accountName: Text(
+                    "Ayush Thaware",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  accountEmail: Text(
+                    "ayushprakashthaware@gmail.com",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage(imageurl),
+                  ),
+                )),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.home,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Home",
+                textScaleFactor: 1.2,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.profile_circled,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Profile",
+                textScaleFactor: 1.2,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.mail,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Email Me",
+                textScaleFactor: 1.2,
+                style: TextStyle(color: Colors.white),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
