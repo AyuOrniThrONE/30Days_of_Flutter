8f1a2a754c92202c6677ebe06fab2114bb1b98d4
AyuOrniThrONE <ayushprakashthaware@gmail.com>
2022-03-25 02:14:20 +0530

    added catalog

---
+++ lib/models/catalog.dart
@@ -0,0 +1,27 @@
+class item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
+}
+
+final products = [
+  item(
+      id: "Codepur001",
+      name: "Iphone 12 Pro",
+      desc: "Apple Iphone 12the generation",
+      price: 999,
+      color: "#33505a",
+      image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
+];
